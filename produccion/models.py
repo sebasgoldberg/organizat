64c2e0b1d341a5e7fa702bf0f3072f5f5af30eb5
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-04-05 20:02:55 -0700

    Aplicación producción agregada al proyecto. Modelo de producción definido. Pruebas sobre el modelo definidas y ejecutadas en forma exitosa.

---
+++ produccion/models.py
@@ -0,0 +1,218 @@
+# coding=utf-8
+from django.db import models
+from django.utils.translation import ugettext
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+
+class Maquina(models.Model):
+  descripcion = models.CharField(max_length=100, verbose_name=_(u'Descripción'), unique=True)
+
+  class Meta:
+    ordering = ['descripcion']
+    verbose_name = _(u"Maquina")
+    verbose_name_plural = _(u"Maquinas")
+
+class Tarea(models.Model):
+  descripcion = models.CharField(max_length=100, verbose_name=_(u'Descripción'), unique=True)
+  tiempo = models.CharField(max_length=100, verbose_name=_(u'Tiempo'),
+    help_text=_(u'Tiempo necesario para realizar la tarea por unidad de producto.'))
+  class Meta:
+    ordering = ['descripcion']
+    verbose_name = _(u"Tarea")
+    verbose_name_plural = _(u"Tareas")
+
+class TareaMaquina(models.Model):
+  """
+  Tareas que puede realizar una máquina.
+  """
+  maquina = models.ForeignKey(Maquina, verbose_name=_(u'Maquina'))
+  tarea = models.ForeignKey(Tarea, verbose_name=_(u'Tarea'))
+
+  class Meta:
+    ordering = ['maquina__descripcion','tarea__descripcion']
+    verbose_name = _(u"Tarea de una máquina")
+    verbose_name_plural = _(u"Tareas por máquinas")
+    unique_together = (('maquina', 'tarea'),)
+
+  def __unicode__(self):
+    return _(u'%(tarea)s en máquina %(maquina)s') % {
+      'tarea': self.tarea.descripcion,
+      'maquina': self.maquina.descripcion,}
+
+class Producto(models.Model):
+  descripcion = models.CharField(max_length=100, verbose_name=_(u'Descripción'), unique=True)
+
+  class Meta:
+    ordering = ['descripcion']
+    verbose_name = _(u"Producto")
+    verbose_name_plural = _(u"Productos")
+
+class TareaProducto(models.Model):
+  """
+  Tareas por las que se compone un producto
+  """
+  producto = models.ForeignKey(Producto, verbose_name=_(u'Producto'))
+  tarea = models.ForeignKey(Tarea, verbose_name=_(u'Tarea'))
+  
+  class Meta:
+    ordering = ['producto__descripcion', 'tarea__descripcion',]
+    verbose_name = _(u"Tarea a realizar en producto")
+    verbose_name_plural = _(u"Tareas por productos")
+    unique_together = (('producto', 'tarea'),)
+
+class TiempoRealizacionTarea(models.Model):
+  """
+  Se completa en forma automática en función de:
+  - Las tareas posibles que puede tener una máquina.
+  - Las tareas necesarias para realizar un producto.
+  El tiempo predeterminado es el tiempo de la tarea indicada.
+  """
+  maquina = models.ForeignKey(Maquina, verbose_name=_(u'Maquina'), editable=False)
+  producto = models.ForeignKey(Producto, verbose_name=_(u'Producto'), editable=False)
+  tarea = models.ForeignKey(Tarea, verbose_name=_(u'Tarea'), editable=False)
+  tiempo = models.DecimalField(
+    max_digits=7, decimal_places=2, verbose_name=_(u'Tiempo'), 
+    help_text=_(u'Tiempo para realizar la tarea en la máquina y producto indicados, por unidad de producto.'))
+  activa = models.BooleanField(default=True, verbose_name=(u'Activa'),
+    help_text=_(u'Indica si la tarea se puede realizar en la máquina para el producto dado.'))
+  
+  class Meta:
+    ordering = ['maquina__descripcion', 'producto__descripcion', 'tarea__descripcion',]
+    verbose_name = _(u"Tiempo de producción")
+    verbose_name_plural = _(u"Tiempos de producción")
+    unique_together = (('maquina', 'producto', 'tarea'),)
+
+class DependenciaTareaProducto(models.Model):
+  """
+  Indica precedencia entre tareas.
+  Validar:
+    - Las tareas no pueden ser la misma tarea.
+    - No puede haber referencia circular.
+  """
+  producto = models.ForeignKey(Producto, verbose_name=_(u'Tarea en máquina'))
+  tarea = models.ForeignKey(Tarea, verbose_name=_(u'La tarea'), 
+    related_name='mis_dependencias')
+  tarea_anterior = models.ForeignKey(Tarea, verbose_name=_(u'Depende de la tarea'),
+    help_text=_(u'Esta tarea debe realizarse antes'),
+    related_name='dependientes_de_mi')
+
+  class Meta:
+    ordering = ['producto__descripcion','tarea__descripcion','tarea_anterior__descripcion']
+    verbose_name = _(u"Dependencia de tareas en producto")
+    verbose_name_plural = _(u"Dependencias de tareas en productos")
+    unique_together = (('producto', 'tarea', 'tarea_anterior'),)
+
+  def clean(self):
+    if self.tarea and self.tarea_anterior:
+
+      if self.tarea.id == self.tarea_anterior.id:
+        raise ValidationError(_(u'Las tareas seleccionadas deben diferir'))
+
+      if self.producto:
+        self.validar_dependencia_circular(self.tarea_anterior, 
+          self.tarea, self.tarea_anterior.descripcion)
+    
+    self.validar_tarea_corresponde_producto(self.tarea)
+    self.validar_tarea_corresponde_producto(self.tarea_anterior)
+
+  def validar_tarea_corresponde_producto(self,tarea):
+    if self.producto and tarea:
+      try:
+        TareaProducto.objects.get(producto=self.producto,tarea=tarea)
+      except TareaProducto.DoesNotExist:
+        raise ValidationError(_(u'La tarea %s no forma parte del proceso de producción del producto %s.') % (
+          tarea.descripcion, self.producto.descripcion))
+
+  def validar_dependencia_circular(self, tarea_inicial, tarea_anterior, camino):
+    dependencias = DependenciaTareaProducto.objects.filter(
+      producto=self.producto, tarea_anterior=tarea_anterior)
+    for dependencia in dependencias:
+      nuevo_camino = '%s -> %s' % (camino, dependencia.tarea_anterior.descripcion)
+      if tarea_inicial.id == dependencia.tarea.id:
+        nuevo_camino = '%s -> %s' % (nuevo_camino, dependencia.tarea.descripcion)
+        raise ValidationError(_(u'Dependencia circular detectada: %s') % nuevo_camino)
+      self.validar_dependencia_circular(tarea_inicial,
+        dependencia.tarea, nuevo_camino)
+
+
+class Pedido(models.Model):
+  descripcion = models.CharField(max_length=100, verbose_name=_(u'Descripción'))
+
+  def __unicode__(self):
+    return u'#%s - %s' % (self.id, self.descripcion)
+
+  class Meta:
+    ordering = ['-id']
+    verbose_name = _(u"Pedido")
+    verbose_name_plural = _(u"Pedidos")
+
+class ItemPedido(models.Model):
+  """
+  Item de un pedido.
+  Validar:
+    - No se puede repetir el producto en 2 items distintos de un mismo pedido.
+  """
+  pedido = models.ForeignKey(Pedido, verbose_name=_(u'Pedido'))
+  producto = models.ForeignKey(Producto, verbose_name=_(u'Producto'))
+  cantidad = models.DecimalField(
+    max_digits=7, decimal_places=2, verbose_name=_(u'Cantidad'))
+
+  class Meta:
+    ordering = ['-pedido__id','producto__descripcion']
+    verbose_name = _(u"Item pedido")
+    verbose_name_plural = _(u"Items pedidos")
+    unique_together = (('pedido', 'producto',),)
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from produccion.models import *
+
+def agregar_combinaciones_tiempos(sender, instance, **kwargs):
+  """
+  Una misma tarea T que este asociada a una maquina M por un lado y a un 
+  producto P por el otro debe generar una entrada (T,M,P)
+  """
+  tarea = instance.tarea
+
+  for tareaMaquina in tarea.tareamaquina_set.all():
+    for tareaProducto in tarea.tareaproducto_set.all():
+      maquina=tareaMaquina.maquina
+      producto=tareaProducto.producto
+      try:
+        TiempoRealizacionTarea.objects.get(tarea=tarea,
+          maquina=maquina, producto=producto)
+      except TiempoRealizacionTarea.DoesNotExist:
+        TiempoRealizacionTarea.objects.create(tarea=tarea,
+          maquina=maquina, producto=producto, tiempo=tarea.tiempo)
+
+post_save.connect(agregar_combinaciones_tiempos, 
+  sender=TareaMaquina)
+post_save.connect(agregar_combinaciones_tiempos, 
+  sender=TareaProducto)
+
+
+def eliminar_combinaciones_tiempos_tarea_maquina(sender, instance, **kwargs):
+  """
+  Se eliminan todas las combinaciones asociadas al par (tarea,maquina)
+  pasado.
+  """
+  tarea = instance.tarea
+  maquina = instance.maquina
+  TiempoRealizacionTarea.objects.filter(tarea=tarea, maquina=maquina).delete()
+
+post_delete.connect(eliminar_combinaciones_tiempos_tarea_maquina, 
+  sender=TareaMaquina)
+
+
+def eliminar_combinaciones_tiempos_tarea_producto(sender, instance, **kwargs):
+  """
+  Se eliminan todas las combinaciones asociadas al par (tarea,maquina)
+  pasado.
+  """
+  tarea = instance.tarea
+  producto = instance.producto
+  TiempoRealizacionTarea.objects.filter(tarea=tarea, producto=producto).delete()
+
+post_delete.connect(eliminar_combinaciones_tiempos_tarea_producto, 
+  sender=TareaProducto)
---
+++ produccion/tests.py
@@ -0,0 +1,216 @@
+# coding=utf-8
+from django.test import TestCase
+from produccion.models import * 
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext
+from django.utils.translation import ugettext_lazy as _
+
+class TiempoRealizacionTareaTestCase(TestCase):
+
+  def setUp(self):
+    M1=Maquina.objects.create(descripcion='M1')
+    M2=Maquina.objects.create(descripcion='M2')
+    M3=Maquina.objects.create(descripcion='M3')
+
+    T1=Tarea.objects.create(descripcion='T1', tiempo=5)
+    T2=Tarea.objects.create(descripcion='T2', tiempo=10)
+    T3=Tarea.objects.create(descripcion='T3', tiempo=3.5)
+
+    """
+          T1    T2    T3
+    M1    X
+    M2    X     X
+    M3          X     X
+    """
+    TareaMaquina.objects.create(tarea=T1,maquina=M1)
+    TareaMaquina.objects.create(tarea=T1,maquina=M2)
+    TareaMaquina.objects.create(tarea=T2,maquina=M2)
+    TareaMaquina.objects.create(tarea=T2,maquina=M3)
+    TareaMaquina.objects.create(tarea=T3,maquina=M3)
+
+    P1=Producto.objects.create(descripcion='P1')
+    P2=Producto.objects.create(descripcion='P2')
+    P3=Producto.objects.create(descripcion='P3')
+
+    """
+          T1    T2    T3
+    P1    X     X
+    P2    X           X
+    P3    X     X     X
+    """
+    TareaProducto.objects.create(tarea=T1,producto=P1)
+    TareaProducto.objects.create(tarea=T2,producto=P1)
+    TareaProducto.objects.create(tarea=T1,producto=P2)
+    TareaProducto.objects.create(tarea=T3,producto=P2)
+    TareaProducto.objects.create(tarea=T1,producto=P3)
+    TareaProducto.objects.create(tarea=T2,producto=P3)
+    TareaProducto.objects.create(tarea=T3,producto=P3)
+
+  def test_completado_automatico_tiempos(self):
+    """
+    Dadas las condiciones iniciales en el método setUp, el modelo
+    TiempoRealizacionTarea debería tener solo las siguientes
+    instancias (12 en total):
+      (M1,T1,P1, 5)
+      (M1,T1,P2, 5)
+      (M1,T1,P3, 5)
+      (M2,T1,P1, 5)
+      (M2,T1,P2, 5)
+      (M2,T1,P3, 5)
+      (M2,T2,P1, 10)
+      (M2,T2,P3, 10)
+      (M3,T2,P1, 10)
+      (M3,T2,P3, 10)
+      (M3,T3,P2, 3.5)
+      (M3,T3,P3, 3.5)
+    """
+    self.assertEqual(TiempoRealizacionTarea.objects.count(),12)
+
+    M1=Maquina.objects.get(descripcion='M1')
+    M2=Maquina.objects.get(descripcion='M2')
+    M3=Maquina.objects.get(descripcion='M3')
+    T1=Tarea.objects.get(descripcion='T1')
+    T2=Tarea.objects.get(descripcion='T2')
+    T3=Tarea.objects.get(descripcion='T3')
+    P1=Producto.objects.get(descripcion='P1')
+    P2=Producto.objects.get(descripcion='P2')
+    P3=Producto.objects.get(descripcion='P3')
+
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P1, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P2, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P3, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M2, tarea=T1, producto=P1, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M2, tarea=T1, producto=P2, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M2, tarea=T1, producto=P3, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M2, tarea=T2, producto=P1, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M2, tarea=T2, producto=P3, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P1, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P3, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P2, tiempo=3.5)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P3, tiempo=3.5)
+
+    """
+    Luego de borrar la maquina M2 deberían quedar las siguientes relaciones:
+      (M1,T1,P1, 5)
+      (M1,T1,P2, 5)
+      (M1,T1,P3, 5)
+      (M3,T2,P1, 10)
+      (M3,T2,P3, 10)
+      (M3,T3,P2, 3.5)
+      (M3,T3,P3, 3.5)
+    """
+    M2.delete()
+
+    self.assertEqual(TiempoRealizacionTarea.objects.count(),7)
+
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P1, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P2, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P3, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P1, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P3, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P2, tiempo=3.5)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P3, tiempo=3.5)
+
+    """
+    Luego de borrar la asociacion entre el producto P1 y la tarea T1 deberían 
+    quedar las siguientes relaciones:
+      (M1,T1,P2, 5)
+      (M1,T1,P3, 5)
+      (M3,T2,P1, 10)
+      (M3,T2,P3, 10)
+      (M3,T3,P2, 3.5)
+      (M3,T3,P3, 3.5)
+    """
+    TareaProducto.objects.filter(tarea=T1,producto=P1).delete()
+
+    self.assertEqual(TiempoRealizacionTarea.objects.count(),6)
+
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P2, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M1, tarea=T1, producto=P3, tiempo=5)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P1, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T2, producto=P3, tiempo=10)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P2, tiempo=3.5)
+    TiempoRealizacionTarea.objects.get(maquina=M3, tarea=T3, producto=P3, tiempo=3.5)
+
+
+class DependenciaTareaProductoTestCase(TestCase):
+
+  def setUp(self):
+    P1=Producto.objects.create(descripcion='P1')
+    P2=Producto.objects.create(descripcion='P2')
+
+    T1=Tarea.objects.create(descripcion='T1', tiempo=5)
+    T2=Tarea.objects.create(descripcion='T2', tiempo=10)
+    T3=Tarea.objects.create(descripcion='T3', tiempo=3.5)
+    T4=Tarea.objects.create(descripcion='T4', tiempo=6)
+    T5=Tarea.objects.create(descripcion='T5', tiempo=7)
+
+    """
+          T1    T2    T3    T4    T5
+    P1    X     X     X     X
+    P2    X     X                 X
+    """
+    TareaProducto.objects.create(tarea=T1,producto=P1)
+    TareaProducto.objects.create(tarea=T2,producto=P1)
+    TareaProducto.objects.create(tarea=T3,producto=P1)
+    TareaProducto.objects.create(tarea=T4,producto=P1)
+    TareaProducto.objects.create(tarea=T1,producto=P2)
+    TareaProducto.objects.create(tarea=T2,producto=P2)
+    TareaProducto.objects.create(tarea=T5,producto=P2)
+
+  def test_verificacion_dependencia_circular(self):
+
+    P1=Producto.objects.get(descripcion='P1')
+    P2=Producto.objects.get(descripcion='P2')
+
+    T1=Tarea.objects.get(descripcion='T1')
+    T2=Tarea.objects.get(descripcion='T2')
+    T3=Tarea.objects.get(descripcion='T3')
+    T4=Tarea.objects.get(descripcion='T4')
+    T5=Tarea.objects.get(descripcion='T5')
+
+    """
+    Se intenta crear la dependencia T1 -> T5 para el producto P1, pero la
+    tarea T5 no forma parte del proceso de producción de P1
+    """
+    try:
+      P1T1T5=DependenciaTareaProducto(producto=P1, tarea_anterior=T1, tarea=T5)
+      P1T1T5.clean()
+      self.fail('No debería dejar crear dependencias entre tareas que no componen un producto.')
+    except ValidationError:
+      pass
+
+    """
+    Para el producto P1 se crean las dependencias:
+      T1 -> T4
+      T3 -> T4
+      T4 -> T2
+    Para el producto P2 se crean las dependencias:
+      T2 -> T1
+    """
+    P1T1T4=DependenciaTareaProducto(producto=P1, tarea_anterior=T1, tarea=T4)
+    P1T1T4.clean()
+    P1T1T4.save()
+    P1T3T4=DependenciaTareaProducto.objects.create(producto=P1, tarea_anterior=T3, tarea=T4)
+    P1T3T4.clean()
+    P1T3T4.save()
+    P1T4T2=DependenciaTareaProducto.objects.create(producto=P1, tarea_anterior=T4, tarea=T2)
+    P1T4T2.clean()
+    P1T4T2.save()
+
+    P2T2T1=DependenciaTareaProducto.objects.create(producto=P2, tarea_anterior=T2, tarea=T1)
+    P2T2T1.clean()
+    P2T2T1.save()
+
+    """
+    Luego al crear la dependencia T2 -> T1 para el producto P1
+    debería ocurrir una excepción.
+    """
+    try:
+      dependencia=DependenciaTareaProducto(producto=P1, tarea_anterior=T2, tarea=T1)
+      dependencia.clean()
+      self.fail(_(u'Falló la validación de referencia circular.'))
+    except ValidationError as e:
+      self.assertEqual('Dependencia circular detectada: T2 -> T1 -> T4 -> T2',e.message)
+
+    self.assertEqual(DependenciaTareaProducto.objects.count(),4)
