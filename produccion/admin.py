6c42b5c09ed3427a0ffe8628f8e97bf025ebb27a
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-08-26 00:04:20 -0300

    Se agrega la posibilidad de edita el tiempo de cada tarea en el listado de tareas.

--- produccion/admin.py
+++ produccion/admin.py
@@ -4,66 +4,67 @@ from produccion.models import *
 
 
 class DependenciaTareaProductoInline(admin.TabularInline):
-  model=DependenciaTareaProducto
+  model = DependenciaTareaProducto
   extra = 1
 
 class TareaMaquinaInline(admin.TabularInline):
-  model=TareaMaquina
+  model = TareaMaquina
   extra = 1
 
 class TareaProductoInline(admin.TabularInline):
-  model=TareaProducto
+  model = TareaProducto
   extra = 1
 
 class MaquinaAdmin(admin.ModelAdmin):
-  inlines=[TareaMaquinaInline]
-  list_display=['id','descripcion']
+  inlines = [TareaMaquinaInline]
+  list_display = ['id', 'descripcion']
   list_display_links = ('id', 'descripcion')
-  search_fields=['descripcion']
+  search_fields = ['descripcion']
   list_per_page = 40
 
 class TareaAdmin(admin.ModelAdmin):
-  inlines=[TareaMaquinaInline,TareaProductoInline]
-  list_display=['id','descripcion','tiempo']
-  list_display_links = ('id', 'descripcion')
-  search_fields=['descripcion']
-  list_per_page = 40
+    inlines = [TareaMaquinaInline, TareaProductoInline]
+    list_display = ['id', 'descripcion', 'tiempo']
+    list_display_links = ('id', 'descripcion')
+    search_fields = ['descripcion']
+    list_editable = ['tiempo', ]
+    list_per_page = 40
 
 class ProductoAdmin(admin.ModelAdmin):
-  inlines=[TareaProductoInline]
-  list_display=['id','descripcion']
+  inlines = [TareaProductoInline]
+  list_display = ['id', 'descripcion']
   list_display_links = ('id', 'descripcion')
-  search_fields=['descripcion']
+  search_fields = ['descripcion']
   list_per_page = 40
 
 class ProductoProxyDependenciasTareasAdmin(admin.ModelAdmin):
-  inlines=[DependenciaTareaProductoInline]
-  list_display=['id','descripcion']
+  inlines = [DependenciaTareaProductoInline]
+  list_display = ['id', 'descripcion']
   list_display_links = ('id', 'descripcion')
-  search_fields=['descripcion']
+  search_fields = ['descripcion']
   list_per_page = 40
 
 class TiempoRealizacionTareaAdmin(admin.ModelAdmin):
-  list_display=['id','maquina', 'producto', 'tarea', 'tiempo', 'activa']
-  list_display_links = ('id', )
-  list_filter=['maquina', 'producto', 'tarea']
-  list_editable=['tiempo', 'activa']
+  list_display = ['id', 'maquina', 'producto', 'tarea', 'tiempo', 'activa']
+  list_display_links = ('id',)
+  list_filter = ['maquina', 'producto', 'tarea']
+  list_editable = ['tiempo', 'activa']
   list_per_page = 40
 
 class ItemPedidoInline(admin.TabularInline):
-  model=ItemPedido
+  model = ItemPedido
   extra = 1
 
 class PedidoAdmin(admin.ModelAdmin):
-  inlines=[ItemPedidoInline]
-  list_display=['id','descripcion']
+  inlines = [ItemPedidoInline]
+  list_display = ['id', 'descripcion']
   list_display_links = ('id', 'descripcion')
-  search_fields=['descripcion']
+  search_fields = ['descripcion']
   list_per_page = 40
 
-admin.site.register(Maquina,MaquinaAdmin)
-admin.site.register(Tarea,TareaAdmin)
-admin.site.register(Producto,ProductoAdmin)
-admin.site.register(ProductoProxyDependenciasTareas,ProductoProxyDependenciasTareasAdmin)
-admin.site.register(TiempoRealizacionTarea,TiempoRealizacionTareaAdmin)
-admin.site.register(Pedido,PedidoAdmin)
+admin.site.register(Maquina, MaquinaAdmin)
+admin.site.register(Tarea, TareaAdmin)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(ProductoProxyDependenciasTareas, ProductoProxyDependenciasTareasAdmin)
+admin.site.register(TiempoRealizacionTarea, TiempoRealizacionTareaAdmin)
+admin.site.register(Pedido, PedidoAdmin)
